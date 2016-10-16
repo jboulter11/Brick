6bbaa531cb5acd0db9e89548c3d922cd04a9054a
Jim Boulter <jboulter11@gmail.com>
2016-10-16 13:24:00 -0400

    stuff test

--- test-app.py
+++ test-app.py
@@ -1,3 +1,6 @@
+# import serial
+import random
+
 from flask import Flask, Response, render_template
 
 import gevent
@@ -8,10 +11,19 @@ gevent.monkey.patch_all()
 app = Flask(__name__)
 
 
+def verlet(r, v, dt, a):
+    r_new = r + v * dt + a(r) * dt ** 2 / 2
+    v_new = v + (a(r) + a(r_new)) / 2 * dt
+    return (r_new, v_new)
+
+
 def event_stream():
     while True:
-        gevent.sleep(1)
-        yield 'data: %s\n\n' % 0
+        gevent.sleep(float(1) / 30)
+        # ser = serial.Serial('/dev/ttyACM0', 9600)
+        # data = eval(ser.readline())
+        str = "{(random.randint(0,100), random.randint(0,100), random.randint(0,100))"+"}"
+        yield 'data: %s\n\n' % (random.randint(0,100), random.randint(0,100), random.randint(0,100))
 
 
 @app.route('/stream')
