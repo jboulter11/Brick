09c6a54893f6beee89705390dc4a08073a970388
Charlie You <charlie@charlieyou.me>
2016-10-16 11:26:49 -0400

    finished app

--- app.py
+++ app.py
@@ -1,14 +1,31 @@
-from flask import Flask
+from flask import Flask, Response, render_template
 
 from position_rotation import get_position, get_rotation
+
+import gevent
+import gevent.monkey
+from gevent.pywsgi import WSGIServer
+gevent.monkey.patch_all()
 
 app = Flask(__name__)
 
 
-class PositionRotation(Resource):
-    def get(self):
-        return {'position': get_position(), 'rotation': get_rotation()}
+def event_stream():
+    while True:
+        yield 'data: %s\n\n' %\
+            {'position': get_position(), 'rotation': get_rotation()}
+
+
+@app.route('/stream')
+def sse_request():
+    return Response(event_stream(), mimetype='text/event-stream')
+
+
+@app.route('/')
+def page():
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    http_server = WSGIServer(('127.0.0.1', 8001), app)
+    http_server.serve_forever()
