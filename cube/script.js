68e47bbff21a61387faab7b2db0e549938ae182c
Jim Boulter <jboulter11@gmail.com>
2016-10-16 10:28:52 -0400

    Add SSE listeners

--- cube/script.js
+++ cube/script.js
@@ -1,6 +1,29 @@
 var xAngle = 0, yAngle = 0;
 console.log("Initilized");
 
+if (!!window.EventSource) {
+  var source = new EventSource('stream.php');
+} else {
+  // Result to xhr polling :(
+}
+
+source.addEventListener('message', function(e) {
+  var data = JSON.parse(e.data);
+  console.log(data.id, data.msg);
+}, false);
+
+source.addEventListener('open', function(e) {
+  // Connection was opened.
+  console.log("Stream Opened");
+}, false);
+
+source.addEventListener('error', function(e) {
+  if (e.readyState == EventSource.CLOSED) {
+    // Connection was closed.
+    console.log("Stream Closed");
+  }
+}, false);
+
 document.addEventListener('keydown', function(e) {
   console.log("keydown", e.keyCode);
   switch(e.keyCode) {
